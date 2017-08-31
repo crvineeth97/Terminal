bd7c79201c2669b7554367c639a1db91a7cf8b5a
Vineeth Chelur <crvineeth97@gmail.com>
2017-08-31 20:41:54 +0530

    Add basic structure. Have to complete echo

--- main.c
+++ main.c
@@ -17,105 +17,104 @@ void spaceAllocationError()
         exit(EXIT_FAILURE);
 }
 
-char *builtin_str[] = {
-        (char*)"cd",
-        (char*)"echo",
-        (char*)"exit",
-        (char*)"help",
-        (char*)"ls",
-        (char*)"pwd",
+char *builtInStrings[] = {
+    (char *)"cd",
+    (char *)"echo",
+    (char *)"help",
+    (char *)"ls",
+    (char *)"pwd",
+    (char *)"texit",
 };
 
-int (*builtin_func[]) (char **) = {
-        &lsh_cd,
-        &lsh_help,
-        &lsh_exit
-};
-
-int lsh_num_builtins() {
-        return sizeof(builtin_str) / sizeof(char *);
-}
-
-/*
-   Builtin function implementations.
- */
-int lsh_cd(char **args)
+int cd(char **args)
 {
-        if (args[1] == NULL) {
-                fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-        } else {
-                if (chdir(args[1]) != 0) {
-                        perror("lsh");
-                }
-        }
+        const struct passwd *pw = getpwuid(getuid());
+        const char *HOME = pw->pw_dir;
+        if (args[1] == NULL)
+                chdir(HOME);
+        else if (chdir(args[1]) != 0)
+                perror("Error");
         return 1;
 }
 
-int lsh_help(char **args)
+int echo(char **args)
 {
-        int i;
-        printf("Stephen Brennan's LSH\n");
+        return 1;
+}
+
+int texit(char **args)
+{
+        return 0;
+}
+
+int help(char **args)
+{
+        int i, n = sizeof(builtInStrings) / sizeof(char *);
         printf("Type program names and arguments, and hit enter.\n");
         printf("The following are built in:\n");
 
-        for (i = 0; i < lsh_num_builtins(); i++) {
-                printf("  %s\n", builtin_str[i]);
-        }
+        for (i = 0; i < n; i++)
+                printf("  %s\n", builtInStrings[i]);
 
         printf("Use the man command for information on other programs.\n");
         return 1;
 }
 
-int lsh_exit(char **args)
+int ls(char **args)
 {
-        return 0;
+        return 1;
 }
 
-char* pwd(bool flg)
+int pwd(char **args)
 {
-        char *cwd = (char*)malloc(sizeof(char) * 9056);
-        char *pwd = (char*)malloc(sizeof(char) * 9056);
-        if (!getcwd(cwd, 9056))
-                cwd = (char*)"Directory";
-        if(flg)
-                return cwd;
-        else
+        char *cwd = (char *)malloc(sizeof(char) * 9056);
+        char *pwd = (char *)malloc(sizeof(char) * 9056);
+        getcwd(cwd, 9056);
+        if (!cwd)
+                cwd = (char *)"Directory";
+        if (strcmp(args[0], "0") == 0)
         {
                 const struct passwd *pw = getpwuid(getuid());
                 const char *HOME = pw->pw_dir;
-                const char* TILDA = "~";
+                const char *TILDA = "~";
                 const size_t HOMELEN = strlen(HOME);
                 size_t i;
-                for(i=0; i<HOMELEN; i++)
-                        if(cwd[i]!=HOME[i])
+                for (i = 0; i < HOMELEN; i++)
+                        if (cwd[i] != HOME[i])
                                 break;
-                if(i==HOMELEN)
+                if (i == HOMELEN)
                 {
                         strcat(pwd, TILDA);
                         strcat(pwd, &cwd[i]);
-                        return pwd;
+                        printf("%s", pwd);
                 }
                 else
-                        return cwd;
+                        printf("%s", cwd);
         }
+        else
+                printf("%s\n", cwd);
+        return 1;
 }
 
 void printPrompt()
 {
         char *username = getlogin();
         if (!username)
-                username = (char*)"Username";
-        char *hostname = (char*)malloc(sizeof(char) * 9056);
+                username = (char *)"Username";
+        char *hostname = (char *)malloc(sizeof(char) * 9056);
         gethostname(hostname, 9056);
-        if(!hostname)
-                hostname = (char*)"Hostname";
-        printf("<%s@%s:%s> ", username, hostname, pwd(0));
+        if (!hostname)
+                hostname = (char *)"Hostname";
+        printf("<%s@%s:", username, hostname);
+        char *zero[] = {"0"};
+        pwd(zero);
+        printf("> ");
 }
 
-char* readLine()
+char *readLine()
 {
         int bufsize = LINEBUFFERSIZE, pos = 0, ch;
-        char *buffer = (char*)malloc(sizeof(char) * bufsize);
+        char *buffer = (char *)malloc(sizeof(char) * bufsize);
 
         if (!buffer)
                 spaceAllocationError();
@@ -134,7 +133,7 @@ char* readLine()
                 if (pos >= bufsize)
                 {
                         bufsize *= LINEBUFFERSIZE;
-                        buffer = (char*)realloc(buffer, bufsize);
+                        buffer = (char *)realloc(buffer, bufsize);
                         if (!buffer)
                                 spaceAllocationError();
                 }
@@ -144,7 +143,7 @@ char* readLine()
 char **tokenizeLine(char *line)
 {
         int bufsize = TOKENBUFFERSIZE, pos = 0;
-        char **tokens = (char **)malloc(bufsize * sizeof(char*));
+        char **tokens = (char **)malloc(bufsize * sizeof(char *));
         char *token;
 
         if (!tokens)
@@ -158,7 +157,7 @@ char **tokenizeLine(char *line)
                 if (pos >= bufsize)
                 {
                         bufsize *= TOKENBUFFERSIZE;
-                        tokens = (char **)realloc(tokens, bufsize * sizeof(char*));
+                        tokens = (char **)realloc(tokens, bufsize * sizeof(char *));
                         if (!tokens)
                                 spaceAllocationError();
                 }
@@ -167,49 +166,48 @@ char **tokenizeLine(char *line)
         return tokens;
 }
 
+int (*builtInFunctions[])(char **) = {
+    &cd,
+    &echo,
+    &help,
+    &ls,
+    &pwd,
+    &texit,
+};
+
 int launchCommand(char **args)
 {
         pid_t pid, wpid;
         int status;
 
         pid = fork();
-        if (pid == 0) {
-                // Child process
-                if (execvp(args[0], args) == -1) {
+        if (pid == 0)
+        {
+                if (execvp(args[0], args) == -1)
                         perror("Error");
-                }
                 exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-                // Error forking
+        }
+        else if (pid < 0)
                 perror("Error forking");
-        } else {
-                // Parent process
-                do {
+        else
+                do
+                {
                         wpid = waitpid(pid, &status, WUNTRACED);
                 } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
 
         return wpid;
 }
 
 int executeCommand(char **args)
 {
-        // int i;
-
-        if (args[0] == NULL) {
-                // An empty command was entered.
+        int i, n = sizeof(builtInStrings) / sizeof(char *);
+        if (args[0] == NULL)
                 return 1;
-        }
-
-        // for (i = 0; i < lsh_num_builtins(); i++) {
-        //         if (strcmp(args[0], builtin_str[i]) == 0) {
-        //                 return (*builtin_func[i])(args);
-        //         }
-        // }
-
+        for (i = 0; i < n; i++)
+                if (strcmp(args[0], builtInStrings[i]) == 0)
+                        return (*builtInFunctions[i])(args);
         return launchCommand(args);
 }
-
 
 void loop()
 {
@@ -217,7 +215,8 @@ void loop()
         int status = 1;
         char **args;
 
-        do {
+        do
+        {
                 printPrompt();
                 line = readLine();
                 args = tokenizeLine(line);
@@ -227,7 +226,6 @@ void loop()
                 free(args);
         } while (status);
 }
-
 
 int main()
 {
